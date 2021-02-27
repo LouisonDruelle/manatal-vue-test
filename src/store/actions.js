f2828cbba8b736b0c4a87fb99b6540f2c8f5f6d5
ldruelle <ldruelle@atecna.fr>
2021-02-27 16:14:01 +0100

    feat: store

---
+++ src/store/actions.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const API_URL = 'https://newsapi.org/v2/';
+const API_KEY = '099148be22804e849a0c6fe022b7cf5e';
+
+export const getHeadlines = ({ commit }) => {
+  axios.get(`${API_URL}top-headlines?country=us&apiKey=${API_KEY}`)
+    .then((response) => {
+      commit('SET_HEADLINES', response.data.articles);
+    });
+};
+
+export const getSources = ({ commit }) => {
+  axios.get(`${API_URL}sources?apiKey=${API_KEY}`)
+    .then((response) => {
+      commit('SET_SOURCES', response.data);
+    });
+};
---
+++ src/store/mutations.js
@@ -0,0 +1,7 @@
+export const SET_HEADLINES = (state, headlines) => {
+  state.headlines = headlines;
+};
+
+export const SET_SOURCES = (state, sources) => {
+  state.sources = sources;
+};
