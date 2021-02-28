361efc7c24b123e1c5f4370129c6e37053e47ef3
ldruelle <ldruelle@atecna.fr>
2021-02-28 14:10:19 +0100

    feat: section title

---
+++ src/components/SectionTitle.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1 class="mb-8 section-title">{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SectionTitle',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped>
+.section-title {
+  font-size: 20px;
+  border-bottom: 1px solid #dddddd;
+}
+</style>
