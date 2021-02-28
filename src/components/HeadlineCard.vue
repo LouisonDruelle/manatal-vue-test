a3ec9a3c94afaf78c7bbebcd6b09c778ccdcf4bc
ldruelle <ldruelle@atecna.fr>
2021-02-28 14:08:01 +0100

    feat: headline list

---
+++ src/components/HeadlineCard.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <v-lazy
+      v-model="isActive"
+      :options="{
+        threshold: .5
+      }"
+      min-height="200"
+      transition="fade-transition"
+    >
+      <v-card
+        class="mx-auto mb-6"
+        max-width="800"
+        @click="addHeadlineToHistory()"
+      >
+        <v-img
+          height="200px"
+          :src="headline.urlToImage"
+        >
+        </v-img>
+
+        <v-card-subtitle class="pb-0">
+          Number 10
+        </v-card-subtitle>
+
+        <v-card-text class="text--primary py-3">
+          <div>{{ headline.title }}</div>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-btn
+            color="orange"
+            text
+          >
+            Read more
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-lazy>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['headline'],
+
+  data: () => ({
+    isActive: false,
+  }),
+
+  methods: {
+    addHeadlineToHistory() {
+      this.$store.dispatch('addHeadlineToHistory', {
+        headline: this.headline,
+      });
+    },
+  },
+};
+</script>
---
+++ src/components/HeadlineList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <SectionTitle msg='Latest headlines'/>
+    <v-row>
+      <v-col cols="12" sm="6" md="4" lg="3" v-for="(headline, index) in headlines" :key="index" >
+        <HeadlineCard :headline="headline" />
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import HeadlineCard from './HeadlineCard.vue';
+import SectionTitle from './SectionTitle.vue';
+
+export default {
+  components: {
+    HeadlineCard,
+    SectionTitle,
+  },
+
+  computed: {
+    headlines() {
+      return this.$store.state.headlines;
+    },
+  },
+
+  mounted() {
+    this.$store.dispatch('getHeadlines');
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
