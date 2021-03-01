541c8489fe956962090ec4dc5db74329103e3481
ldruelle <ldruelle@atecna.fr>
2021-03-01 22:31:13 +0100

    feat: homepage spinner

---
+++ src/components/Spinner.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="mx-auto my-5" style="width:70px">
+    <v-progress-circular
+      :size="70"
+      :width="7"
+      color="purple"
+      indeterminate
+    ></v-progress-circular>
+  </div>
+</template>
--- src/components/headline/HeadlineList.vue
+++ src/components/headline/HeadlineList.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <SectionTitle msg='Latest headlines'/>
+    <Spinner v-if="loading && headlines.length == 0"/>
     <ErrorAlert v-if="error" :error="error"/>
     <v-row v-else>
       <v-col cols="12" sm="6" md="4" lg="3" v-for="(headline, index) in headlines" :key="index" >
@@ -13,12 +14,14 @@
 <script>
 import SectionTitle from '../SectionTitle.vue';
 import ErrorAlert from '../ErrorAlert.vue';
+import Spinner from '../Spinner.vue';
 import HeadlineCard from './HeadlineCard.vue';
 
 export default {
   components: {
     SectionTitle,
     ErrorAlert,
+    Spinner,
     HeadlineCard,
   },
 
@@ -38,11 +41,13 @@ export default {
     this.loading = true;
     this.error = '';
     try {
-      await this.$store.dispatch('getHeadlines');
+      await setTimeout(() => {
+        this.$store.dispatch('getHeadlines');
+        this.loading = false;
+      }, 3000);
     } catch (e) {
       this.error = e;
     }
-    this.loading = false;
   },
 };
 </script>
