330700ba976c392ce986e8b943a5748d124d60e8
ldruelle <ldruelle@atecna.fr>
2021-03-02 22:10:47 +0100

    feat: gradient on img

--- src/components/PresentationCard.vue
+++ src/components/PresentationCard.vue
@@ -4,6 +4,7 @@
       <v-img
         class="white--text align-end"
         height="300px"
+        gradient="to top right, rgba(100,115,201,.33), rgba(25,32,72,.7)"
         src="https://images.unsplash.com/photo-1546422904-90eab23c3d7e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80"
       >
       <v-card-title class="display-1 font-weight-black">
