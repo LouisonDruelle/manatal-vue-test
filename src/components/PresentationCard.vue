fc5911e29ed61c96deec8eb90400dc494e572115
ldruelle <ldruelle@atecna.fr>
2021-03-02 22:04:25 +0100

    fix: presentation card style

--- src/components/PresentationCard.vue
+++ src/components/PresentationCard.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
     <v-card class="mx-auto mb-6">
-      <v-card-title class="text--primary font-weight-bold">
-        SqwaNews. All you need to know in 5min !
-      </v-card-title>
       <v-img
-        height="280px"
+        class="white--text align-end"
+        height="300px"
         src="https://images.unsplash.com/photo-1546422904-90eab23c3d7e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80"
       >
+      <v-card-title class="display-1 font-weight-black">
+        SqwaNews. Everything you need to know !
+      </v-card-title>
       </v-img>
     </v-card>
   </div>
