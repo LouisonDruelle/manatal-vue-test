1d5f304700df3b5e70fb1a677a177b47c4c17a90
ldruelle <ldruelle@atecna.fr>
2021-03-02 22:04:54 +0100

    feat: hover on headline cards

--- src/components/headline/HeadlineCard.vue
+++ src/components/headline/HeadlineCard.vue
@@ -8,49 +8,54 @@
       min-height="200"
       transition="fade-transition"
     >
-      <v-card
-        class="mx-auto mb-6"
-        max-width="800"
-        style="height:410px"
-      >
-        <v-img
-          v-if="headline.urlToImage"
-          height="200px"
-          :src="headline.urlToImage"
+      <v-hover>
+      <template v-slot:default="{ hover }">
+        <v-card
+          :elevation="hover ? 24 : 6"
+          class="mx-auto mb-6"
+          max-width="800"
+          style="height:410px"
         >
-        </v-img>
-
-        <v-img
-          v-else
-          height="200px"
-          src="https://images.unsplash.com/photo-1506702315536-dd8b83e2dcf9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
-        >
-        </v-img>
-
-        <v-card-subtitle class="pb-0">
-          {{ headline.publishedAt | date }}
-        </v-card-subtitle>
-
-        <v-card-text class="text--primary py-3 subtitle-2" style="height:110px">
-          <div>{{ headline.title }}</div>
-        </v-card-text>
-        <v-card-actions>
-          <v-btn
-            @click="editTitle()"
-            color="purple"
-            text
+          <v-img
+            v-if="headline.urlToImage"
+            height="200px"
+            :src="headline.urlToImage"
           >
-            Edit title
-          </v-btn>
-          <v-btn
-            @click="getHeadlineDetails()"
-            color="purple"
-            text
+          </v-img>
+
+          <v-img
+            v-else
+            height="200px"
+            src="https://images.unsplash.com/photo-1506702315536-dd8b83e2dcf9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
           >
-            Read more
-          </v-btn>
-        </v-card-actions>
-      </v-card>
+          </v-img>
+
+          <v-card-subtitle class="pb-0">
+            {{ headline.publishedAt | date }}
+          </v-card-subtitle>
+
+          <v-card-text class="text--primary py-3 subtitle-2" style="height:110px">
+            <div>{{ headline.title }}</div>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn
+              @click="editTitle()"
+              color="purple"
+              text
+            >
+              Edit title
+            </v-btn>
+            <v-btn
+              @click="getHeadlineDetails()"
+              color="purple"
+              text
+            >
+              Read more
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </template>
+      </v-hover>
     </v-lazy>
     <HeadlineDialog :headline="headline" :dialog="dialog" @close="dialog = false"/>
   </div>
