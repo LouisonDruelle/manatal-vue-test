9df1b4915dd267ab0ea2dea942436c91a5a3d75c
ldruelle <ldruelle@atecna.fr>
2021-03-14 17:03:38 +0100

    feat: navbar drawer

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,35 +1,42 @@
 <template>
   <div>
     <v-app-bar
-      color="deep-purple accent-4"
+      color="deep-purple"
       dark
     >
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
       <v-toolbar-title @click="$router.push('/')">SqwaNews.</v-toolbar-title>
-
-      <v-spacer></v-spacer>
-      <v-menu>
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            icon
-            v-bind="attrs"
-            v-on="on"
-          >
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
-        </template>
-
-        <v-list>
-          <v-list-item
-            v-for="(item, i) in routes"
-            :key="i"
-            link
-            @click="$router.push(item.route)"
-          >
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
     </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <template v-for="(item, i) in routes">
+            <v-list-item v-if="$route.name !== item.title"
+              :key="i"
+              link
+              @click="$router.push(item.route)"
+            >
+              <v-list-item-icon>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item>
+          </template>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
   </div>
 </template>
 
@@ -37,9 +44,12 @@
 export default {
   data: () => ({
     routes: [
-      { title: 'History', route: '/history' },
-      { title: 'Error example', route: '/error' },
+      { title: 'Home', route: '/', icon: 'mdi-home' },
+      { title: 'History', route: '/history', icon: 'mdi-history' },
+      { title: 'ErrorExample', route: '/error', icon: 'mdi-alert' },
     ],
+    drawer: false,
+    group: null,
   }),
 };
 </script>
